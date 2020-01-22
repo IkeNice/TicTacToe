90fb22a893774a7b204ec37ec6107a1c5f7b2837
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-01-22 17:31:05 +0300

    more settings

--- src/com/data/ApplicationData.java
+++ src/com/data/ApplicationData.java
@@ -3,7 +3,23 @@ package com.data;
 public class ApplicationData {
     public static final String PLAYER_VS_PLAYER = "player";
     public static final String PLAYER_VS_COMPUTER = "computer";
+    
+    private static final String PLAYERX = "Player X";
+    private static final String PLAYERO = "Player O";
 
     public static String playMode = PLAYER_VS_PLAYER;
+    private static String currentPlayer;
+    private static boolean gameOver;
 
+    // возвращаем имя текущего игрока
+    public static String getCurrentPlayerName() {
+        if (currentPlayer == null) setDefaultSettings();
+        return currentPlayer;
+    }
+
+    // установка настроек игры по умолчанию
+    private static void setDefaultSettings() {
+        currentPlayer = PLAYERX;
+        gameOver = false;
+    }
 }
--- src/com/view/PlayView.java
+++ src/com/view/PlayView.java
@@ -2,6 +2,7 @@ package com.view;
 
 import com.Main;
 import com.controller.PlayViewController;
+import com.data.ApplicationData;
 import com.sun.corba.se.impl.io.OptionalDataException;
 import com.view.components.GameCell;
 
@@ -10,6 +11,7 @@ import java.awt.*;
 
 public class PlayView extends Panel {
 
+    private Font buttonFont;
     private GameCell[][] cells;
     private PlayViewController controller;
 
@@ -52,11 +54,58 @@ public class PlayView extends Panel {
         topPanel.add(playAgain);
         topPanel.add(settings);
 
+        // создаем экземпляры кнопок-ячеек, добавляем слушателя и
+        // размещаем их на дополнительной панели
         for (int i = 0; i < cells.length; i++){
             for (int j = 0; j < cells.length; j++){
                 cells[i][j] = new GameCell(i, j);
-            }
+                cells[i][j].addMouseListener(controller);
+                cells[i][j].setFont(buttonFont);
 
-        }
+                cellsPanel.add(cells[i][j]);
+            }
+        } // end for
+
+        // задаем выравнивение для поля-комментария
+        comment.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // размещаем все компоненты на основной панели
+        this.add(topPanel, BorderLayout.NORTH);
+        this.add(cellsPanel, BorderLayout.CENTER);
+        this.add(comment, BorderLayout.SOUTH);
+
+        // сбрасываем состояние кнопок-ячеек на начальное
+        reset();
     } // end constructor PlayView
+
+    // сброс состояния-ячеек на начальное
+    private void reset() {
+        // делаем ячейки активными
+        setCellsEnabled(true);
+
+        // делаем ячейки пустыми
+        for (GameCell[] line : cells){
+            for (GameCell item : line) {
+                item.setText("");
+                item.setBackground(null);
+            }
+        }
+        // получаем имя текущего игрока и
+        // устанавливаем его в поле комментария
+        setCurrentPlayerNote(ApplicationData.getCurrentPlayerName());
+    } // end method reset
+
+    // устанавливаем примечание об очереди хода в поле комментария
+    private void setCurrentPlayerNote(String currentPlayerName) {
+        comment.setText(currentPlayerName + " your turn.");
+    }
+
+    // установка активности кнопок-ячеек формы
+    private void setCellsEnabled(boolean b) {
+        for (int i = 0; i < cells.length; i++){
+            for (int j = 0; j < cells.length; j++){
+                cells[i][j].setEnabled(b);
+            }
+        }
+    } // end method setCellsEnabled
 }
